85221fd9492a238c3f8c85c2f53084ac9155a515
Chimeratech-py <xkenntsx619@gmail.com>
2022-06-02 15:22:06 +0800

    new lappy

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -17,8 +18,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseAuthorization();
+
 
 app.MapControllers();
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Account } from '../_models/account';
+import { ACCOUNTS } from '../_data/mock-account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
+  accountsAvailable: Account[] = ACCOUNTS;
   constructor() { }
+
+  login(inputAccount: Account){
+    this.accountsAvailable.forEach( (item) => {
+      if(item.username === inputAccount.username && item.password === inputAccount.password){
+       
+      }
+    })
+  }
 }
